141d62ca5c166067a95ebd770d986ee0f3338be8
amanymous <aman.mirza9@yahoo.com>
2021-08-02 14:06:05 +0500

    feat: jotto testing redux app

--- jotto/src/components/Congrats.test.js
+++ jotto/src/components/Congrats.test.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import EnzymeAdapter,{ shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttr,checkPropTypes } from '../../test/testUtils';
 import Congrats from './Congrats'
 
-EnzymeAdapter.configure({ adapter: new EnzymeAdapter() })
+const defaultProps = { success: false }
 
-const setup = (props={})=>{
-    return shallow(<Congrats {...props}/>)
-}
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-test('renders without error',()=>{
-     
+/**
+* Functional react component for congratulatory message.
+* @function
+* @param {object} props - React props.
+* @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+*/
+
+const setup = (props = {}) => {
+    const setupProps = {...defaultProps,...props}
+    return shallow(<Congrats {...setupProps} />)
+} 
+
+test('renders without error', () => {
+    const wrapper = setup()
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.length).toBe(1)
+
 })
 
-test('render no text when `success` prop is false',()=>{
+test('render no text when `success` prop is false', () => {
+    const wrapper = setup({ success: false })
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.text()).toBe('')
+})
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+    const wrapper = setup({ success: true })
+    const message = findByTestAttr(wrapper, 'congrats-message')
+    expect(message.text().length).not.toBe(1)
 
 })
 
-test('renders non-empty congrats message when `success` prop is true',()=>{
-
-})  
+test('does not throw warning with the expected props', () => {
+    const expectedProps = { success: false }
+    expect(Congrats,expectedProps)
+})

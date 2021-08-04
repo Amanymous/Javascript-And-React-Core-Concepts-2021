d235ba297a5f363c81006a6530b5253c46696d95
amanymous <aman.mirza9@yahoo.com>
2021-08-04 10:20:31 +0500

    feat: ESLint basics

---
+++ ESLint/index.js
@@ -0,0 +1,16 @@
+import { foo } from "./test"
+// import React from "react"
+
+// class hello extends React.Component{
+//     render(){
+//         return <div>hi</div>
+//     }}
+// export default hello
+var age = 22
+function hi() {
+    return `${foo} and  ${age}`
+}
+hi()
+/* eslint-disable-next-line no-console */
+console.log(hi())
+console.log("yo")
